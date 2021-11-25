aadf7d1a3ce682278964a27492144ca44cc78382
Marcin Orzechowski <marcinorz@o2.pl>
2021-11-25 19:14:27 +0100

    Add project files.

---
+++ PicturesAnalysis/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using PicturesAnalysis.Utils;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PicturesAnalysis
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void OpenFileClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|PNG Image|*.png";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Orginal.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                AfterFiltering.Source = Orginal.Source;
+            }
+        }
+
+        private void Slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Orginal.LayoutTransform = new ScaleTransform(e.NewValue, e.NewValue);
+        }
+
+        private void Slider2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            AfterFiltering.LayoutTransform = new ScaleTransform(e.NewValue, e.NewValue);
+        }
+
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+            BitmapImage image = Orginal.Source as BitmapImage;
+            double percens;
+            AfterFiltering.Source = ConvertBitmapToBitmapImage(BitmapAnalyser.Calculate(image,out percens));
+            Result.Text = (Math.Round(percens * 100,3)).ToString() + "%";
+        }
+
+        private BitmapImage ConvertBitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+    }
+
+}
---
+++ PicturesAnalysis/Utils/BitmapAnalyser.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace PicturesAnalysis.Utils
+{
+    public static class BitmapAnalyser
+    {
+        private static Bitmap GetBitmapFromBitmapImage(BitmapImage image)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(image));
+                enc.Save(outStream);
+                Bitmap bitmap = new Bitmap(outStream);
+
+                return new Bitmap(bitmap);
+            }
+        }
+
+        public static Bitmap Calculate(BitmapImage image, out double percents)
+        {
+            var bitmap = GetBitmapFromBitmapImage(image);
+            var copy = new Bitmap(bitmap);
+            bitmap = AverageFilter(bitmap); // filtr usredniajacy
+            for (int i = 10; i < bitmap.Width - 10; i += 10) //sprawdzenie maską
+                for (int j = 10; j < bitmap.Height - 10; j += 10)
+                {
+                    var greenWin = 0;
+                    for (int x = i - 10; x <= i; x++)
+                        for (int y = j - 10; y <= j; y++)
+                        {
+                            var color = bitmap.GetPixel(x, y);
+                            if (color.G > color.R && color.G > color.B)
+                            {
+                                greenWin++;
+                            }
+                        }
+                    if (greenWin < 50)
+                    {
+                        for (int x = i - 10; x <= i; x++)
+                            for (int y = j - 10; y <= j; y++)
+                            {
+                                var color = bitmap.GetPixel(x, y);
+                                bitmap.SetPixel(x, y, Color.FromArgb(color.R, 0, color.B));
+                            }
+                    }
+                }
+
+            var pixels = bitmap.Width * bitmap.Height;
+            var counter = 0;
+            for (int i = 0; i < bitmap.Width; i++)
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    var color = bitmap.GetPixel(i, j);
+                    if (2*color.G -color.R - color.B > 15 && color.G > 10)
+                    {
+                        counter++;
+                        copy.SetPixel(i, j, Color.FromArgb(Math.Min(color.R + 150,255), color.G, color.B));
+                    }
+                }
+            percents = counter / (double)pixels;
+            return copy;
+        }
+
+
+
+        private static Color[,] getColors(Bitmap bitmap)
+        {
+            Color[,] colors = new Color[bitmap.Width, bitmap.Height];
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    colors[i, j] = bitmap.GetPixel(i, j);
+                }
+            }
+
+            return colors;
+        }
+        public static Bitmap AverageFilter(Bitmap bitmap)
+        {
+            double[,] mask = new double[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    mask[i, j] = 1.0 / 9.0;
+                }
+            }
+
+            var colors = getColors(bitmap);
+            Bitmap newBitmap = new Bitmap(bitmap);
+
+            for (int i = 3; i < bitmap.Width - 3; i++)
+            {
+                for (int j = 3; j < bitmap.Height - 3; j++)
+                {
+                    newBitmap.SetPixel(i, j, GetColorValueLinear(i, j, mask, colors));
+                }
+            }
+            return newBitmap;
+        }
+        private static Color GetColorValueLinear(int x, int y, double[,] mask, Color[,] colors)
+        {
+            var R = 0;
+            var G = 0;
+            var B = 0;
+            var maskI = 0;
+            var maskJ = 0;
+            for (int i = x - 1; i <= x + 1; i++)
+            {
+                maskJ = 0;
+                for (int j = y - 1; j <= y + 1; j++)
+                {
+                    R += (int)(colors[i, j].R * mask[maskI, maskJ]);
+                    G += (int)(colors[i, j].G * mask[maskI, maskJ]);
+                    B += (int)(colors[i, j].B * mask[maskI, maskJ]);
+                    maskJ++;
+                }
+                maskI++;
+            }
+            R = R < 0 ? 0 : R > 255 ? 255 : R;
+            G = G < 0 ? 0 : G > 255 ? 255 : G;
+            B = B < 0 ? 0 : B > 255 ? 255 : B;
+            return Color.FromArgb(R, G, B);
+        }
+
+
+    }
+}
